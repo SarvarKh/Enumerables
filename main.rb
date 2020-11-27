b2f73339b630516d3cc3ed71151644dd30ab9539
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-11-27 20:17:45 +0500

    Steps 1 and 2 was added to the my_all method

--- main.rb
+++ main.rb
@@ -34,36 +34,40 @@ module Enumerable
     true_counter = 0;
 
     # 1-step Check if array elemenets are all true
-    self.my_each do |element|
-      if !!element
-        true_counter1 += 1
+    def step1()
+      length_array = self.length
+      true_counter1 = 0;
+      self.my_each do |element|
+        if !!element
+          true_counter1 += 1
+        end
       end
+      is_true_without_block = true_counter1 == length_array ? initial_value = true : initial_value = false
+      initial_value
     end
-    is_true_without_block = true_counter1 == length_array ? initial_value = true : initial_value = false
-    
 
-    # 2-step Check if all array elements is in line with block condiction
-    self.my_each do |x|
-      if yield(x)
-        true_counter += 1
-      end
-    end
-    is_true = true_counter == length_array ? initial_value = true : initial_value = false
-    
-
-    # 3-step
+    # 2-step Check if there is block given
     if block_given?
-      is_true_without_block
+      length_array = self.length
+      true_counter = 0;
+      self.my_each do |x|
+        if yield(x)
+          true_counter += 1
+        end
+      end
+      is_true = true_counter == length_array ? initial_value = true : initial_value = false
     else
-      is_true
+      step1()
     end
 
-    initial_value
+    # 3-step Check if all array elements is in line with block condiction
+
+    
+
   end
 end
 
-[1, 2, 3, 4, 5].my_all? # => true
-# [1, 2, 3, false].my_all? # => false
+[1, 2, 3].my_all?(Integer) # => true
 
 
 
@@ -100,22 +104,22 @@ end
 # puts
 
 # # 4. my_all? (example test cases)
-# puts 'my_all?'
-# puts '-------'
-# p [3, 5, 7, 11].my_all?(&:odd?) # => true
-# p [-8, -9, -6].my_all? { |n| n < 0 } # => true
-# p [3, 5, 8, 11].my_all?(&:odd?) # => false
-# p [-8, -9, -6, 0].my_all? { |n| n < 0 } # => false
-# # test cases required by tse reviewer
-# p [1, 2, 3, 4, 5].my_all? # => true
-# p [1, 2, 3, false].my_all? # => false
-# p [1, 2, 3].my_all?(Integer) # => true
-# p %w[dog door rod blade].my_all?(/d/) # => true
-# p [1, 1, 1].my_all?(1) # => true
-# false_block = proc { |n| n<5 }
-# p (1..5).my_all?(&false_block) # false
-# p [1, 2.2, 3, 0.6].my_all?( ) #=> True
-# puts
+puts 'my_all?'
+puts '-------'
+p [3, 5, 7, 11].my_all?(&:odd?) # => true
+p [-8, -9, -6].my_all? { |n| n < 0 } # => true
+p [3, 5, 8, 11].my_all?(&:odd?) # => false
+p [-8, -9, -6, 0].my_all? { |n| n < 0 } # => false
+# test cases required by tse reviewer
+p [1, 2, 3, 4, 5].my_all? # => true
+p [1, 2, 3, false].my_all? # => false
+p [1, 2, 3].my_all?(Integer) # => true
+p %w[dog door rod blade].my_all?(/d/) # => true
+p [1, 1, 1].my_all?(1) # => true
+false_block = proc { |n| n<5 }
+p (1..5).my_all?(&false_block) # false
+p [1, 2.2, 3, 0.6].my_all?( ) #=> True
+puts
 
 # # 5. my_any? (example test cases)
 # puts 'my_any?'
